e4f9c63ce19be29ef466285786422316021c3329
nilar <nicolas.larche@rca.fr>
2016-09-16 14:03:42 +0200

    add $init chat-room

--- src/component/chat-room/chat-room.component.js
+++ src/component/chat-room/chat-room.component.js
@@ -2,25 +2,28 @@ import template from './chat-room.html';
 import Styles from './chat-room.css';
 
 class controller {
-     constructor(ChatService) {
-        this.chatService = ChatService;
-        this.styles = Styles;
-    }
-    update($event) {
-        this.chatService.add($event);
-        this.list = this.chatService.get();
-    }
+  constructor(ChatService) {
+    this.chatService = ChatService;
+    this.styles = Styles;
+  }
+  $onInit() {
+    this.list = this.chatService.get();
+  }
+  update($event) {
+    this.chatService.add($event);
+    this.list = this.chatService.get();
+  }
 }
 
 // $inject for dependency injection when minification
 controller.$inject = ['ChatService'];
 
 const chatRoomComponent = {
-    template,
-    controller,
-    bindings : {
-        user: '@'
-    }
+  template,
+  controller,
+  bindings: {
+    user: '@'
+  }
 };
 
 
